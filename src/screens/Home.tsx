f97d223edb2eeb97c0404ccbff605451e983d650
kesantos <kesantos@paschoalotto.com.br>
2023-01-22 03:03:44 -0300

    feat(ProgressBar): Criado componente progressBar

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,3 +1,4 @@
+import { useNavigation } from "@react-navigation/native";
 import { View, Text, ScrollView } from "react-native";
 import { HabitDay, DAY_SIZE } from "../components/HabitDay";
 import { Header } from "../components/Header";
@@ -11,6 +12,7 @@ const amountOfDaysToFill = minimumSummaryDatesSizes - datesFromYearStart.length;
 
 
 export function Home() {
+  const { navigate } = useNavigation()
   return (
     <View className="flex-1 bg-background px-8 pt-16">
       <Header />
@@ -37,6 +39,7 @@ export function Home() {
           {
             datesFromYearStart.map(date => (
               <HabitDay key={date.toISOString()}
+                onPress={() => navigate('habit', { date: date.toISOString() })}
               />
             ))
           }
