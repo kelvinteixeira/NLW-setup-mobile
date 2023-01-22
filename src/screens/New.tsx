e02eb38cf93daf246bba60b463cfbdb4cb48f99d
kesantos <kesantos@paschoalotto.com.br>
2023-01-22 02:37:46 -0300

    feat(Button): Criado botão de confirmar hábito

--- src/screens/New.tsx
+++ src/screens/New.tsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
-import { View, ScrollView, Text, TextInput } from "react-native";
+import { View, ScrollView, Text, TextInput, TouchableOpacity } from "react-native";
 import { BackButton } from "../components/Backbutton";
 import { Checkbox } from "../components/Checkbox";
+import { Feather } from '@expo/vector-icons'
+import colors from "tailwindcss/colors";
 
 const avalibleWeekDays = [
   'Domingo',
@@ -24,7 +26,11 @@ export function New() {
   }
   return (
     <View className="flex-1 bg-background px-8 pt-16">
-      <ScrollView showsVerticalScrollIndicator={false}>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 100}}
+      
+      >
         <BackButton />
 
         <Text className="mt-6 text-white font-extrabold text-3xl" >
@@ -37,6 +43,8 @@ export function New() {
 
         <TextInput
           className="h-12 pl-4 rounded-lg mt-3 bg-zinc-800 text-white focus: border-2 focus:border-green-600"
+          placeholder="Exercícios, dormir em, etc..."
+          placeholderTextColor={colors.zinc[400]}
         />
 
         <Text className="font-semibold mt-4 mb-3 text-white text-base">
@@ -53,6 +61,22 @@ export function New() {
             />
           ))
         }
+
+        <TouchableOpacity
+          className="w-full h-14 flex-row items-center justify-center bg-green-600 rounded-md mt-6"
+          activeOpacity={0.7}
+        >
+          <Feather
+            name="check"
+            size={20}
+            color={colors.white}
+          />
+
+
+          <Text className="font-semibold text-base text-white ml-2">
+            Confirmar
+          </Text>
+        </TouchableOpacity>
       </ScrollView>
     </View>
   )
